0261de7aa438cde23e093c1940da2fc319e7e84b
Anton Kozytskyi <kozytskyi@zfort.com>
2016-06-27 19:30:07 +0300

    Removed the plugins installation.

--- services/jenkins/custom.groovy
+++ services/jenkins/custom.groovy
@@ -4,4 +4,5 @@ def jobName = "php-template"
 def configXml = new URL("https://raw.githubusercontent.com/sebastianbergmann/php-jenkins-template/master/config.xml").getText()
 def xmlStream = new ByteArrayInputStream(configXml.getBytes())
 
+// TODO: don't create a job if already exists
 Jenkins.instance.createProjectFromXML(jobName, xmlStream)
